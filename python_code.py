77bc24a3659f297ba20855fb8eae9480452004c4
atvey124 <matveygghhgg@gmail.com>
2024-02-10 19:51:50 +0300

    bag fix

--- python_code.py
+++ python_code.py
@@ -1,44 +1,37 @@
 from flask import Flask, render_template
 
 
+
 app = Flask(__name__)
+
 
 
 @app.route("/")
 
-def main_str():
-    return render_template("basic.html",title = "Главная страница 2.0")
-
+def main():
+    return render_template("basic.html", title = "основная страница")
 
 @app.route("/info")
+
 def info():
-    return "Информация о сайте"
-
-@app.route("/hello/<name>")
-
-def hello(name):
-    return f"Hello {name}"
-
-@app.route("/calc/<int:x>/<int:y>")
-
-def calc(x,y):
-    return f"{x} + {y} = {x + y}"
+    return render_template("info.html", title = "Страница с информацией")
 
 @app.route("/people")
 
 def people():
-    
-    list_people = ["gf","fdgg","fdg","gdgfd","dgfgd"]
-    
-    return render_template("people.html",title = "Люди",data = list_people)
+    list_people = ["gfgf","gfhg","ftgf","hgfh","fhfj"]
+    return render_template("list_people.html",title = "Страница пользователей",data = list_people)
 
 @app.route("/comm")
+
 def comm():
-    list_comment = ["blyy","gcfg","gfh","gfhg","hfhf"]
-    return render_template("comment.html",title = "Комментарии",data = list_comment)
-    
+    list_com = ["dfgfh","gfhg","hfgfh","hfgfh","hffh"]
+    return render_template("comment.html", title = "Комментарии",data = list_com)
 
+@app.route("/hello/<name>")
 
+def hello(name):
+    return render_template("hello.html", title = "Приветствие",name1 = name)
 
 if __name__ == "__main__":
     app.run()
